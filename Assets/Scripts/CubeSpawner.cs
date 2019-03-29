b62de1e2c0520ce5d6541968b9fe5d3108816da6
vikkymaurya <vikkymaurya@gmail.com>
2019-03-29 11:39:01 +0530

    Initial commit

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    PoolSpawner poolSpawner;
+
+    private void Start()
+    {
+        poolSpawner = PoolSpawner.Instance;
+    }
+
+    void FixedUpdate()
+    {
+        poolSpawner.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PoolSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+       public string tag;
+       public GameObject prefab;
+       public int size;
+    }
+
+    public static PoolSpawner Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public List<Pool> pools = new List<Pool>();
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)    
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+
+    public GameObject SpawnFromPool(string tag,Vector3 position, Quaternion rotation)
+    {
+        if(!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with Tag " + tag + "Does't exist");
+            return null;
+        }
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        IPoolObjecct poolObjecct = objectToSpawn.GetComponent<IPoolObjecct>();
+        if(poolObjecct != null)
+        {
+            poolObjecct.OnObjectSpawn();
+        }
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
